1718fbd4461d4e91bc4e0584ab66745c341e1653
Lion Chen <lion@Lions-MacBook-Air.local>
2014-10-28 23:47:03 +0800

    next step is to implemetn gameboard.moveCurrentCardToNext, moveCurrentCardToPrevious, moveCurrentCardAcrossNext, moveCurrentCardAcrossPrevious, flipCurrentCard

--- app/assets/javascripts/views/cards/list.js
+++ app/assets/javascripts/views/cards/list.js
@@ -1,3 +1,26 @@
 var CardsView = Marionette.CollectionView.extend({
-	childView: CardView
+	childView: CardView,
+
+	onBeforeRender: function() {
+		$(document).keydown(function(event){ 
+			switch(event.which) {
+				case 37: 
+					alert('left');
+					break;
+				case 38: 
+					alert('up');
+					break;
+				case 39: 
+					alert('right');
+					break;				
+				case 40: 
+					alert('down');
+					break;
+				case 13:
+					alert('enter');
+					break;
+			}
+		});
+	},
+
 });
