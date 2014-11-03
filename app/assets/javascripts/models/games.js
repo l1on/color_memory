b06e382b7648889f83b6aeb9e4354d5c89300cd7
Lion Chen <lion@Lions-MacBook-Air.local>
2014-11-03 21:18:05 +0800

    added a compare func

--- app/assets/javascripts/models/games.js
+++ app/assets/javascripts/models/games.js
@@ -1,5 +1,12 @@
 ColorMemory.Models.Games = Backbone.Collection.extend({
 	model: ColorMemory.Models.Game,
 	
-	url: '/games'
+	url: '/games',
+
+	comparator: function(gameA, gameB) {
+		if (gameA.get('score') < gameB.get('score')) return 1;
+		if (gameA.get('score') > gameB.get('score')) return -1;
+		return 0;
+	}
+
 });
